23bf7f47488168854a8bb0abcd230b0a23391d8f
Imam <imamnurfahudi@gmail.com>
2024-08-15 09:01:25 +0700

    chore: menambahkan primevue sebagai ui library

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,45 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+
+import Aura from '@primevue/themes/aura';
+import PrimeVue from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
+
+import '@/assets/styles.scss';
+import '@/assets/tailwind.css';
+import { definePreset } from '@primevue/themes';
+
+const app = createApp(App);
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{emerald.50}',
+      100: '{emerald.100}',
+      200: '{emerald.200}',
+      300: '{emerald.300}',
+      400: '{emerald.400}',
+      500: '{emerald.500}',
+      600: '{emerald.600}',
+      700: '{emerald.700}',
+      800: '{emerald.800}',
+      900: '{emerald.900}',
+      950: '{emerald.950}'
+    }
+  }
+});
+
+app.use(PrimeVue, {
+  theme: {
+    preset: MyPreset,
+    options: {
+      darkModeSelector: '.app-dark'
+    }
+  }
+});
+app.use(ToastService);
+app.use(ConfirmationService);
+
+app.mount('#app');
