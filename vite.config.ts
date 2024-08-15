cd3f6d19c9b33cd940830c26ceb840d2cf2fab68
Imam <imamnurfahudi@gmail.com>
2024-08-15 09:10:55 +0700

    chore: settings dasar vite untuk project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+
+import Vue from '@vitejs/plugin-vue';
+
+import Tailwind from 'tailwindcss';
+import Autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  // Settings css untuk vite
+  css: {
+    postcss: {
+      plugins: [Tailwind(), Autoprefixer()]
+    }
+  },
+  plugins: [
+    Vue({
+      features: {
+        // Mengaktifkan propsDestructure untuk vue
+        propsDestructure: true
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    // Mengaktifkan warning apabila ada file dengan size lebih dari 5000KB
+    chunkSizeWarningLimit: 5000
+  },
+  optimizeDeps: {
+    // Mengoptimasi file vue
+    entries: ['./src/**/*.vue']
+  },
+  server: {
+    // Aplikasi akan berjalan di port 3001
+    port: 3001
+  }
+});
